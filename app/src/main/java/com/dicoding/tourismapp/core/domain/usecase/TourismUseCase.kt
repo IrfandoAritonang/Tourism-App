3539fc7035ca553255a8e2e8080d56495faebbfb
Irfando <irfandoaritonang86@gmail.com>
2022-08-24 14:46:28 +0700

    done with RXJava

--- app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismUseCase.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismUseCase.kt
@@ -3,9 +3,10 @@ package com.dicoding.tourismapp.core.domain.usecase
 import androidx.lifecycle.LiveData
 import com.bumptech.glide.load.engine.Resource
 import com.dicoding.tourismapp.core.domain.model.Tourism
+import io.reactivex.Flowable
 
 interface TourismUseCase {
-    fun getAllTourism(): LiveData<com.dicoding.tourismapp.core.data.Resource<List<Tourism>>>
-    fun getFavoriteTourism(): LiveData<List<Tourism>>
+    fun getAllTourism(): Flowable<com.dicoding.tourismapp.core.data.Resource<List<Tourism>>>
+    fun getFavoriteTourism(): Flowable<List<Tourism>>
     fun setFavoriteTourism(tourism: Tourism, state: Boolean)
 }

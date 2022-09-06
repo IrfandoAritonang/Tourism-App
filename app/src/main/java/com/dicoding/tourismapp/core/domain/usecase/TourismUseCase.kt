dd93c2972e9587fc7427454b78e41ec96a672288
Irfando <irfandoaritonang86@gmail.com>
2022-09-06 22:23:20 +0700

    coroutine flow done and app done

--- app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismUseCase.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismUseCase.kt
@@ -1,12 +1,11 @@
 package com.dicoding.tourismapp.core.domain.usecase
 
-import androidx.lifecycle.LiveData
 import com.bumptech.glide.load.engine.Resource
 import com.dicoding.tourismapp.core.domain.model.Tourism
-import io.reactivex.Flowable
+import kotlinx.coroutines.flow.Flow
 
 interface TourismUseCase {
-    fun getAllTourism(): Flowable<com.dicoding.tourismapp.core.data.Resource<List<Tourism>>>
-    fun getFavoriteTourism(): Flowable<List<Tourism>>
+    fun getAllTourism(): Flow<com.dicoding.tourismapp.core.data.Resource<List<Tourism>>>
+    fun getFavoriteTourism(): Flow<List<Tourism>>
     fun setFavoriteTourism(tourism: Tourism, state: Boolean)
 }
